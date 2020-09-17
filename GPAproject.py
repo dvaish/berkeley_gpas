288d1c77f4fbb3dcfe21161d107f64d64cebe1a5
dvaish <41309250+dvaish@users.noreply.github.com>
2020-09-16 22:38:52 -0700

    Update GPAproject.py

--- GPAproject.py
+++ GPAproject.py
@@ -24,8 +24,29 @@ class GPA_college:
         plt.legend()
         plt.show()
 
+class GPA_major:
 
+    def __init__(self, data_file, *args):
+        self.data = data_file
+        self.college = args
+        self.majors = (self.data.loc[self.data['College/School'] == args]['Major'] if self.college else self.data['Major']).unique().tolist()
 
+    def average_gpa(self, major):
+        parse_target = self.data.loc[self.data['Major'] == major, ['College/School','Major', 'Average GPA', 'Headcount']]
+        if self.college:
+            parse_target = parse_target.loc[parse_target['College/School'] == self.college]
+        average_gpa = sum(parse_target['Average GPA'].mul(parse_target['Headcount'], fill_value=0) / sum(parse_target['Headcount']))
+        return average_gpa
+
+    def dict_to_df(self):
+        gpa_dictionary = dict(zip(self.majors, [self.average_gpa(i) for i in self.majors]))
+        gpa_dataframe = pd.DataFrame(gpa_dictionary, index=['GPA']).swapaxes('index','columns').sort_values('GPA', ascending=False)
+        return gpa_dataframe
+    
+    def top_gpas(self):
+        head = int(input('How many majors?: '))
+        frame_list = self.dict_to_df().head(head)
+        return frame_list
 
 
     
